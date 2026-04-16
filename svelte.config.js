d3c0274742448a5bb89f77e16c0f5e5bff12ca00
Luis Rivera <lcriverar@unincca.edu.co>
2026-04-15 23:35:24 -0500

    Pipeline CI/CD y configuracion

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static'; // <--- Cambiado de 'auto' a 'static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// Esto genera archivos .html en lugar de necesitar un servidor
+			fallback: '404.html' 
+		}),
+		paths: {
+			// REEMPLAZA 'prueba-actividad-2' si tu repo tiene otro nombre
+			base: process.env.NODE_ENV === 'production' ? '/prueba-actividad-2' : '',
+		}
 	}
 };
 
